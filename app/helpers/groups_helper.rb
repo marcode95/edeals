9a87384c1985bbc223d87c595cfcbe9dff73faec
Marco <marcoerhardt95@gmail.com>
2021-03-05 11:53:14 +0100

    style out group show page header

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,2 +1,13 @@
 module GroupsHelper
+  def group_transaction_sum
+    sum = 0
+    @group.deals.each do |deal|
+      sum += deal.amount
+    end
+    eurofy(sum)
+  end
+
+  def group_transaction_count
+    @group.deals.count
+  end
 end
