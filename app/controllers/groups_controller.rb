7e2c8fad9ab8e31007b9b3da38d702c53cd9c872
Marco <marcoerhardt95@gmail.com>
2021-03-07 16:44:26 +0100

    add add transaction functionality in groups show page

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,28 +1,22 @@
 class GroupsController < ApplicationController
   before_action :set_group, only: %i[show edit update destroy]
-
   # GET /groups or /groups.json
   def index
     @groups = Group.all.order(name: :asc)
   end
-
   # GET /groups/1 or /groups/1.json
   def show
     @groupdeals = @group.deals
   end
-
   # GET /groups/new
   def new
     @group = Group.new
   end
-
   # GET /groups/1/edit
-  def edit; end
-
+  def edit;end
   # POST /groups or /groups.json
   def create
     @group = Group.new(group_params.merge(user_id: current_user.id))
-
     respond_to do |format|
       if @group.save
         format.html { redirect_to @group, notice: 'Group was successfully created.' }
@@ -33,20 +27,14 @@ class GroupsController < ApplicationController
       end
     end
   end
-
   # PATCH/PUT /groups/1 or /groups/1.json
   def update
-    respond_to do |format|
-      if @group.update(group_params)
-        format.html { redirect_to @group, notice: 'Group was successfully updated.' }
-        format.json { render :show, status: :ok, location: @group }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @group.errors, status: :unprocessable_entity }
-      end
-    end
+    selected_id = params[:group][:other_transaction_ids]
+    transaction = Transaction.find(selected_id)
+    group = Group.find(params[:id])
+    group.deals << transaction
+    redirect_to group_path(group)
   end
-
   # DELETE /groups/1 or /groups/1.json
   def destroy
     @group.destroy
@@ -55,14 +43,11 @@ class GroupsController < ApplicationController
       format.json { head :no_content }
     end
   end
-
   private
-
   # Use callbacks to share common setup or constraints between actions.
   def set_group
     @group = Group.find(params[:id])
   end
-
   # Only allow a list of trusted parameters through.
   def group_params
     params.require(:group).permit(:name, :image)
