91399d2ffa4f8a1d3b39c0c7deed9d2135c4b418
Marco <marcoerhardt95@gmail.com>
2021-03-08 09:41:02 +0100

    Fix bug

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,8 +8,8 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require jquery3
-require popper
-require bootstrap-sprockets
+//require popper
+//require bootstrap-sprockets
 
 
 
